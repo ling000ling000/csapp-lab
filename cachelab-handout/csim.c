c82047a50dc35c43bb2db797e02b7e226a71edd2
kk <koohf0823@gmail.com>
2022-11-08 20:46:03 +0800

    implement read parameters

--- cachelab-handout/csim.c
+++ cachelab-handout/csim.c
@@ -1,7 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h> // 文件操作
+#include <getopt.h> // 解析命令行选项参数
+
 #include "cachelab.h"
 
-int main()
+int verbose = 0, s, E, b, S, B;
+
+void usage(void)
 {
-    printSummary(0, 0, 0);
-    return 0;
+    exit(1);
+}
+
+FILE *opt(int argc, char* argv[])
+{
+    FILE *tracefile;
+
+    for (int i = 1; i < argc; i ++ )
+    {
+        int op = getopt(argc, argv, "hvsEbt");
+        switch(op)
+        {
+            case 's': // 组数的位
+                s = atoi(argv[optind]);
+                if (s <= 0) usage();
+                S = 1 << s;
+            case 'E': // 每一组的行数
+                E = atoi(argv[optind]);
+                if (E <= 0) usage();
+                break;
+            case 'b':
+                b = atoi(argv[optind]);
+                if (b <= 0) usage();
+                B = 1 << b;
+                break;
+            case 't': // 文件
+                tracefile = fopen(argv[optind], "r");
+                if (tracefile == NULL) usage();
+                break;
+        }
+    }
+    return tracefile;
+}
+
+void init()
+{
+
+}
+
+int main(int argc, char* argv[])
+{
+    FILE *fd = opt(argc, argv);
+    init();
+    
 }
