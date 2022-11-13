e35d98d728004641e528a0048891f8adba1e124e
kk <koohf0823@gmail.com>
2022-11-13 21:36:00 +0800

    implement 64*64 & 61*67

--- cachelab-handout/trans.c
+++ cachelab-handout/trans.c
@@ -20,7 +20,11 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
  *     be graded. 
  */
 char transpose_submit_desc[] = "Transpose submission";
+
 void trans_32(int M, int N, int A[N][M], int B[M][N]);
+void trans_64(int M, int N, int A[N][M], int B[M][N]);
+void trans_61(int M, int N, int A[N][M], int B[M][N]);
+
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
     switch (M)
@@ -28,12 +32,12 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N])
     case 32:
         trans_32(M, N, A, B);
         break;
-    // case 64:
-    //     trans_64();
-    //     break;
-    // case 61:
-    //     trans_61();
-    //     break;
+    case 64:
+        trans_64(M, N, A, B);
+        break;
+    case 61:
+        trans_61(M, N, A, B);
+        break;
     }
 }
 
@@ -64,6 +68,91 @@ void trans_32(int M, int N, int A[N][M], int B[M][N])
                 B[j + 6][ii] = tmp7;
                 B[j + 7][ii] = tmp8;
             }
+        }
+    }
+}
+
+void trans_64(int M, int N, int A[N][M], int B[M][N])
+{
+    int i, j, x, y;
+    int x1, x2, x3, x4, x5, x6, x7, x8;
+    for (i = 0; i < N; i += 8 )
+    {
+        for (j = 0; j < M; j += 8 )
+        {
+            // 1块8*8，我们分成4块来做，每一块4*4
+            // 处理左上
+            for (x = i; x < i + 4; x ++ ) 
+            {
+                x1 = A[x][j]; // A左上
+                x2 = A[x][j+1]; 
+                x3 = A[x][j+2]; 
+                x4 = A[x][j+3]; 
+
+                x5 = A[x][j+4]; // A右上
+                x6 = A[x][j+5]; 
+                x7 = A[x][j+6]; 
+                x8 = A[x][j+7];  
+                
+                B[j][x] = x1; // 翻转，A左上存到B左上
+                B[j+1][x] = x2; 
+                B[j+2][x] = x3; 
+                B[j+3][x] = x4;
+
+                B[j][x+4] = x5; // 将A右上,存到B的右上
+                B[j+1][x+4] = x6; 
+                B[j+2][x+4] = x7; 
+                B[j+3][x+4] = x8;
+            }
+            // 处理左下
+            for (y = j; y < j + 4; y ++ )
+            {
+                x1 = A[i+4][y]; // A左下
+                x2 = A[i+5][y]; 
+                x3 = A[i+6][y]; 
+                x4 = A[i+7][y];
+
+                x5 = B[y][i+4]; // B右上，也就是之前的A右上
+                x6 = B[y][i+5]; 
+                x7 = B[y][i+6]; 
+                x8 = B[y][i+7];
+                
+                B[y][i+4] = x1; // A左下翻转到B右上
+                B[y][i+5] = x2; 
+                B[y][i+6] = x3; 
+                B[y][i+7] = x4;
+
+                B[y+4][i] = x5; // B右上 赋值到 B左下， 这样A右上也完成了翻转
+                B[y+4][i+1] = x6; 
+                B[y+4][i+2] = x7; 
+                B[y+4][i+3] = x8;
+            }
+            // 处理右下
+            for (x = i + 4; x < i + 8; x ++ )
+            {
+                x1 = A[x][j+4]; // 第四块没有任何改动，和原来效果是一样的
+                x2 = A[x][j+5]; 
+                x3 = A[x][j+6]; 
+                x4 = A[x][j+7];
+
+                B[j+4][x] = x1; 
+                B[j+5][x] = x2; 
+                B[j+6][x] = x3; 
+                B[j+7][x] = x4;
+            }
+        }
+    }
+}
+
+void trans_61(int M, int N, int A[N][M], int B[M][N])
+{
+    int i, j;
+    int ex = 17;  // 看别人选17
+    for (i = 0; i < N; i += ex) {      // 枚举每ex行
+        for (j = 0; j < M; j += ex) {  // 枚举每ex列
+            // 下面开始转置这个ex * ex的块
+            for (int x = i; x < N && x < i + ex; ++x)
+                for (int y = j; y < M && y < j + ex; ++y) B[y][x] = A[x][y];
         }
     }
 }
